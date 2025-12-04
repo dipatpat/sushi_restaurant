b633c55083383c0c125bac43b8ce32e4d9d76d92
Patrycja Szpakowska <patrycja.szpakowska@gmail.com>
2025-12-04 12:22:10 +0100

    Update Guest tests with reservation association

--- SushiRestaurantTests/GuestTests.cs
+++ SushiRestaurantTests/GuestTests.cs
@@ -5,6 +5,13 @@ namespace sushi_restaurant_tests
     [TestFixture]
     public class GuestTests
     {
+        [SetUp]
+        public void SetUp()
+        {
+            Guest.ClearExtent();
+            Reservation.ClearExtent();
+        }
+
         [Test]
         public void Should_Assign_FirstName_LastName_And_Nickname()
         {
@@ -33,6 +40,30 @@ namespace sushi_restaurant_tests
             Assert.That(guest.Nickname, Is.Null);
             Assert.That(guest.FirstName, Is.EqualTo("Nick"));
             Assert.That(guest.LastName, Is.EqualTo("Moon"));
+        }
+
+        [Test]
+        public void New_Guest_Should_Have_Empty_Reservations_Collection()
+        {
+            var guest = new Guest("Anna", "Nowak");
+
+            Assert.That(guest.Reservations, Is.Empty);
+        }
+
+        [Test]
+        public void Creating_Reservation_Associates_It_With_Guest_And_Updates_Reverse()
+        {
+            var guest = new Guest("Anna", "Nowak");
+
+            var reservation = new Reservation(
+                DateTime.Now.AddHours(3),
+                numberOfGuests: 2,
+                guest: guest);
+
+            Assert.That(reservation.Guest, Is.SameAs(guest));
+
+            Assert.That(guest.Reservations, Has.Count.EqualTo(1));
+            Assert.That(guest.Reservations.First(), Is.SameAs(reservation));
         }
     }
 }
