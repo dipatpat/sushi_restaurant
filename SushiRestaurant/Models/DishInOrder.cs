56379aeb7862a60be6e05505eb39e3948a12f516
Patrycja Szpakowska <patrycja.szpakowska@gmail.com>
2025-12-08 09:02:48 +0100

    Add DishInOrder association class

---
+++ SushiRestaurant/Models/DishInOrder.cs
@@ -0,0 +1,44 @@
+namespace SushiRestaurant;
+
+public class DishInOrder
+{
+    private static readonly List<DishInOrder> _extent = new();
+    public static IReadOnlyList<DishInOrder> Extent => _extent.AsReadOnly();
+    public static void ClearExtent() => _extent.Clear();
+
+    private Dish _dish = default!;
+    public Dish Dish => _dish;
+
+    private Order _order = default!;
+    public Order Order => _order;
+
+    public int Quantity { get; private set; }
+    public DateTime TimeOrdered { get; }
+
+    public DishInOrder(Dish dish, Order order, int quantity)
+    {
+        if (dish is null)
+            throw new ArgumentNullException(nameof(dish));
+        if (order is null)
+            throw new ArgumentNullException(nameof(order));
+        if (quantity <= 0)
+            throw new ArgumentOutOfRangeException(nameof(quantity),
+                "Quantity must be positive.");
+
+        _dish = dish;
+        _order = order;
+        Quantity = quantity;
+        TimeOrdered = DateTime.Now;
+
+        _extent.Add(this);
+    }
+
+    public void ChangeQuantity(int newQuantity)
+    {
+        if (newQuantity <= 0)
+            throw new ArgumentOutOfRangeException(nameof(newQuantity),
+                "Quantity must be positive.");
+
+        Quantity = newQuantity;
+    }
+}
